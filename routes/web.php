9596ecf8274b5935686a5a2f7a68a3247a0a2344
JcOlacs <olacojohnclifford@gmail.com>
2024-11-14 22:22:23 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController; // to use and connect this folder inside
+
+
+Route::get('/', [StudentController::class, 'index'])->name('students.index');
+
+Route::get('/create', [StudentController::class, 'create'])->name('students.create');
+
+Route::post('/store', [StudentController::class, 'store'])->name('students.store');
+
+Route::get('/{id}', [StudentController::class, 'show'])->name('students.show');
+
+Route::get('/{id}/edit', [StudentController::class, 'edit'])->name('students.edit');
+
+Route::put('/{id}', [StudentController::class, 'update'])->name('students.update');
